253109931fef86a3b1fc93e0fecce94f6111c825
Maria Melnik <maria.melnik.a@gmail.com>
2017-09-10 00:20:39 +0300

    Create controller for login action

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -7,11 +7,29 @@ use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class SecurityController extends Controller
 {
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $authUtils
+     * @return Response
+     */
+    public function loginAction(AuthenticationUtils $authUtils)
+    {
+        $error = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
     /**
      * @Route("/signup", name="signup")
      * @param Request $request
