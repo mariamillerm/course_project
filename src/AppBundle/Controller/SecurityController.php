d6020b00a40008e19363f60ea98012f1e20be280
Maria Melnik <maria.melnik.a@gmail.com>
2017-09-12 11:54:09 +0300

    Add sending email to user

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +31,22 @@ class SecurityController extends Controller
     }
 
     /**
+     * @Route("/activate/{token}", name="activate")
+     * @param string $token
+     * @return Response
+     */
+    public function activateAccountAction(string $token)
+    {
+        $message = 'Hello, ' . $token;
+        $message = $message . 'Your account is confirmed. Please, login.';
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => '',
+            'error' => $message,
+        ]);
+    }
+
+    /**
      * @Route("/signup", name="signup")
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
@@ -45,10 +61,14 @@ class SecurityController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
+            $user->setRole('ROLE_USER');
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
+
+            //TO DO: use mailer service
+            $this->get('app.email_support')->sendActivationEmail($user);
 
             return $this->redirectToRoute('homepage');
         }
