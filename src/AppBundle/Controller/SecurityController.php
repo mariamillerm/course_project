b704c048b6c4173cf085cceb8de480b74d76d65b
Maria <maria.melnik.a@gmail.com>
2017-09-17 02:31:26 +0300

    Update security functions with services

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -12,7 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
@@ -42,18 +41,10 @@ class SecurityController extends Controller
      */
     public function activateAccountAction(string $token)
     {
-        $em = $this->getDoctrine()->getManager();
-        $token = $em
-            ->getRepository(ConfirmationToken::class)
-            ->findOneBy([
-                'token' => $token,
-            ]);
+        $token = $this->get('app.token_service')->findConfirmationToken($token);
 
         if ($token !== null) {
-            $user = $token->getUser();
-            $user->setIsActive(true);
-            $em->remove($token);
-            $em->flush();
+            $this->get('app.user_service')->activateUser($token);
 
             return $this->render('security/registration_success.html.twig', [
                 'message' => 'Your account is confirmed. Please, login.',
@@ -76,55 +67,27 @@ class SecurityController extends Controller
     public function forgotPasswordAction(Request $request)
     {
         $user = new User();
-
         $form = $this->createForm(ForgotPasswordType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            //TODO: put this in service
-            $em = $this->getDoctrine()->getManager();
-            $user = $form->getData();
-            $user = $em
-                ->getRepository(User::class)
-                ->findOneBy([
-                    'email' => $user->getEmail(),
-                ]);
+            $user = $this->get('app.user_service')->findUserByEmail($form->getData());
 
-            $token = $em
-                ->getRepository(ResetToken::class)
-                ->findOneBy(['user' => $user]);
-            if ($token != null) {
-                $em->remove($token);
-                $em->flush();
+            if ($user === null) {
+                return $this->render('security/registration_success.html.twig', [
+                    'message' => 'Something is going wrong there!',
+                ]);
             }
 
-            $userToken = new ResetToken();
-            $userToken->setUser($user);
-            $userToken->setToken(md5(openssl_random_pseudo_bytes(32)));
-
-            $em->persist($userToken);
-            $em->flush();
-
-            $this
-                ->get('app.email_support')
-                ->sendRecoveryEmail($user, $userToken);
+            $this->get('app.token_service')->setResetTokenToUser($user);
 
             return $this->redirectToRoute('homepage');
-        }
-
-        $errors = $form->getErrors();
-        $error = $errors->current();
-
-        $message = null;
-
-        if ($error !== false) {
-            $message = $error->getMessage();
         }
 
         return $this->render(
             'security/forgot_password.html.twig', [
                 'form' => $form->createView(),
-                'error' => $message,
+                'error' => $this->get('app.form_service')->getFormErrorMessage($form),
             ]
         );
     }
@@ -134,24 +97,15 @@ class SecurityController extends Controller
      *
      * @param string $token
      * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
      *
      * @return Response
      */
-    public function resetPasswordAction(string $token, Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function resetPasswordAction(string $token, Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $token = $em
-            ->getRepository(ResetToken::class)
-            ->findOneBy([
-                'token' => $token,
-            ]);
+        $token = $this->get('app.token_service')->findResetToken($token);
 
-        //TODO: put this in service
         if ($token !== null) {
-            if (!$this->get('app.token_service')->isResetTokenActive($token)) {
-                $em->remove($token);
-                $em->flush();
+            if ($this->get('app.token_service')->isResetTokenDisabled($token)) {
 
                 return $this->render('security/registration_success.html.twig', [
                     'message' => 'Something is going wrong there!',
@@ -163,27 +117,15 @@ class SecurityController extends Controller
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
-                $password = $passwordEncoder->encodePassword($user, $user->getPassword());
-                $user->setPassword($password);
-                $em->remove($token);
-                $em->flush();
+                $this->get('app.user_service')->resetUserPassword($user, $token);
 
                 return $this->redirectToRoute('homepage');
             }
 
-            $errors = $form->getErrors();
-            $error = $errors->current();
-
-            $message = null;
-
-            if ($error !== false) {
-                $message = $error->getMessage();
-            }
-
             return $this->render(
-                'emails/reset_password.html.twig', [
+                'security/reset_password_type.html.twig', [
                     'form' => $form->createView(),
-                    'error' => $message,
+                    'error' => $this->get('app.form_service')->getFormErrorMessage($form),
                 ]
             );
         } else {
