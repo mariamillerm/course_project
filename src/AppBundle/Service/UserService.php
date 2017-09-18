1a621ba2e3944c0e8f44953251a3864126d19205
Maria <maria.melnik.a@gmail.com>
2017-09-18 21:16:29 +0300

    Delete unnecessary function

--- src/AppBundle/Service/UserService.php
+++ src/AppBundle/Service/UserService.php
@@ -72,21 +72,6 @@ class UserService
 
     /**
      * @param User $user
-     *
-     * @return User|object
-     */
-    public function findUser(User $user)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $user = $em
-            ->getRepository(User::class)
-            ->find($user);
-
-        return $user;
-    }
-
-    /**
-     * @param User $user
      * @param string $role
      */
     public function changeUserRole(User $user, string $role)
