55baaeeac70f0cf1cc43164fa453e6904884daa5
Maria <maria.melnik.a@gmail.com>
2017-09-19 11:04:29 +0300

    Update logic of generating hash in token service

--- src/AppBundle/Service/TokenService.php
+++ src/AppBundle/Service/TokenService.php
@@ -2,196 +2,28 @@
 
 namespace AppBundle\Service;
 
-use AppBundle\Entity\ConfirmationToken;
-use AppBundle\Entity\ResetToken;
-use AppBundle\Entity\User;
-use Symfony\Component\DependencyInjection\Container;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 
 class TokenService
 {
     /**
-     * @var Container
+     * @var Registry
      */
-    private $container;
+    private $doctrine;
 
     /**
-     * @param Container $container
+     * @param Registry $doctrine
      */
-    public function __construct(Container $container)
+    public function __construct(Registry $doctrine)
     {
-        $this->container = $container;
+        $this->doctrine = $doctrine;
     }
 
     /**
-     * @param ResetToken $token
-     *
-     * @return bool
+     * @return string
      */
-    public function isResetTokenDisabled(ResetToken $token)
+    public function generateToken(): string
     {
-        $date = $token->getTokenCreateTime();
-        $now = new \DateTime();
-        $interval = $now->diff($date);
-
-        $isDisabled = $interval->h >= 24;
-
-        if ($isDisabled) {
-            $this->removeResetToken($token);
-        }
-
-        return $isDisabled;
-    }
-
-    /**
-     * @param User $user
-     */
-    public function setResetTokenToUser(User $user)
-    {
-        $this->deleteOldResetToken($user);
-        $userToken = $this->createResetToken($user);
-        $this->insertResetTokenInDB($userToken);
-
-        $this
-            ->container
-            ->get('app.email_support')
-            ->sendRecoveryEmail($user, $userToken);
-    }
-
-    /**
-     * @param ResetToken $token
-     */
-    private function insertResetTokenInDB(ResetToken $token)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $em->persist($token);
-        $em->flush();
-    }
-
-    /**
-     * @param User $user
-     */
-    private function deleteOldResetToken(User $user)
-    {
-        $token = $this->findResetTokenByUser($user);
-
-        if ($token !== null) {
-            $this->removeResetToken($token);
-        }
-    }
-
-    /**
-     * @param ResetToken $token
-     */
-    public function removeResetToken(ResetToken $token)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $em->remove($token);
-        $em->flush();
-    }
-
-    /**
-     * @param int $id
-     */
-    public function deleteConfirmationToken(int $id)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $token = $em
-            ->getRepository(ConfirmationToken::class)
-            ->findOneBy(
-                ['user' => $id]
-            );
-
-        if ($token !== null) {
-            $this->removeConfirmationToken($token);
-        }
-    }
-
-    /**
-     * @param ConfirmationToken $token
-     */
-    public function removeConfirmationToken(ConfirmationToken $token)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $em->remove($token);
-        $em->flush();
-    }
-
-    /**
-     * @param string $token
-     *
-     * @return ResetToken|object
-     */
-    public function findResetToken(string $token)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $token = $em
-            ->getRepository(ResetToken::class)
-            ->findOneBy([
-                'token' => $token,
-            ]);
-
-        return $token;
-    }
-
-    /**
-     * @param string $token
-     *
-     * @return ConfirmationToken|object
-     */
-    public function findConfirmationToken(string $token)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $token = $em
-            ->getRepository(ConfirmationToken::class)
-            ->findOneBy([
-                'token' => $token,
-            ]);
-
-        return $token;
-    }
-
-    /**
-     * @param User $user
-     *
-     * @return ResetToken|object
-     */
-    private function findResetTokenByUser(User $user)
-    {
-        $em = $this->container->get('doctrine')->getManager();
-        $token = $em
-            ->getRepository(ResetToken::class)
-            ->findOneBy([
-                'user' => $user,
-            ]);
-
-        return $token;
-    }
-
-    /**
-     * @param User $user
-     *
-     * @return ResetToken
-     */
-    private function createResetToken(User $user)
-    {
-        $token = new ResetToken();
-        $token->setUser($user);
-        $token->setToken(md5(openssl_random_pseudo_bytes(32)));
-
-        return $token;
-    }
-
-    /**
-     * @param User $user
-     *
-     * @return ConfirmationToken
-     */
-    public function createConfirmationToken(User $user)
-    {
-        $userToken = new ConfirmationToken();
-        $userToken->setUser($user);
-        $userToken->setToken(md5(openssl_random_pseudo_bytes(32)));
-
-        return $userToken;
+        return md5(openssl_random_pseudo_bytes(32));
     }
 }
