2bed1fe67b4cdd6a300342181b3d982cb55cebb5
Maria <maria.melnik.a@gmail.com>
2017-09-19 19:24:11 +0300

    Create user provider to login with email or username

---
+++ src/AppBundle/Security/User/UserProvider.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Security\User;
+
+use AppBundle\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface
+{
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    /**
+     * UserProvider constructor.
+     *
+     * @param Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must throw UsernameNotFoundException if the user is not
+     * found.
+     *
+     * @param string $username The username
+     *
+     * @return UserInterface
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByUsername($username)
+    {
+        $user = $this
+            ->doctrine
+            ->getRepository(User::class)
+            ->findUser($username);
+
+        if ($user !== null) {
+            return $user;
+        }
+
+        throw new UsernameNotFoundException('user.not_found');
+    }
+
+    /**
+     * Refreshes the user.
+     *
+     * It is up to the implementation to decide if the user data should be
+     * totally reloaded (e.g. from the database), or if the UserInterface
+     * object can just be merged into some internal array of users / identity
+     * map.
+     *
+     * @param UserInterface $user
+     *
+     * @return UserInterface
+     *
+     * @throws UnsupportedUserException if the user is not supported
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if ($user instanceof User){
+            return $this
+                ->doctrine
+                ->getRepository(User::class)
+                ->find($user->getId());
+        } else {
+            throw new UnsupportedUserException('user.invalid_class');
+        }
+    }
+
+    /**
+     * Whether this provider supports the given user class.
+     *
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return $class === User::class;
+    }
+}
