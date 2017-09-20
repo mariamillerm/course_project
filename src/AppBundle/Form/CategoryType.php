d53e7f0d5cda7041e2cbadcaf2bff9c66bebe82d
Eroshevich Palina <30950652+palinom@users.noreply.github.com>
2017-09-20 14:43:12 +0300

    Update category form

--- src/AppBundle/Form/CategoryType.php
+++ src/AppBundle/Form/CategoryType.php
@@ -2,8 +2,7 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Entity\Category
-use Symfony\Component\Form\AbstractType;
+3use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EntityType;
@@ -21,12 +20,12 @@ class CategoryType extends AbstractType{
 
 		$builder
 		    ->add('category', TextType::class)
-            ->add('parent', EntityType::class, array()
+            ->add('parent', EntityType::class, [
                   'class' => 'AppBundle:Category',
                   'choice_label' => function ($category) {
                   
-                  return $category->getParent());
-
+                  return $category->getParent();
+            }])
             ->add('save', SubmitType::class, array(
              'attr'      => array('class' => 'button-link save'),
              'label'     => 'Save'
