060c4ecb91fa69178c8fc80d31c2f9d8efcf008c
Eroshevich Palina <30950652+palinom@users.noreply.github.com>
2017-09-20 14:44:01 +0300

    Update category form

--- src/AppBundle/Form/CategoryType.php
+++ src/AppBundle/Form/CategoryType.php
@@ -2,9 +2,8 @@
 
 namespace AppBundle\Form;
 
-3use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
