d9fd11ad7efd90659af2e0fcf8dba4d5650371ad
mimiminimalizm <maria.melnik.a@gmail.com>
2017-09-21 20:45:20 +0300

    Create Post repository

---
+++ src/AppBundle/Repository/PostRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use AppBundle\Entity\Post;
+use Doctrine\ORM\EntityRepository;
+
+class PostRepository extends EntityRepository
+{
+    /**
+     * @param \DateTime $latestExecution
+     *
+     * @return Post[]
+     */
+    public function findNewPosts(\DateTime $latestExecution): array
+    {
+        return $this
+            ->createQueryBuilder('p')
+            ->where('p.creationDate > ?1')
+            ->orderBy('p.rating', 'asc')
+            ->setMaxResults(5)
+            ->getQuery()
+            ->setParameter(1, $latestExecution)
+            ->getResult();
+    }
+
+    /**
+     * @return \Doctrine\ORM\Query
+     */
+    public function getPostsQuery()
+    {
+        return $this
+            ->createQueryBuilder('p')
+            ->orderBy('p.id', 'asc')
+            ->getQuery();
+    }
+}
