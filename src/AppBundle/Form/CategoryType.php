c10ba074757371b80a8bcc6ef89d8341d3e2d711
Maria <maria.melnik.a@gmail.com>
2017-09-22 13:47:27 +0300

    Update form for new presentation of category entity

--- src/AppBundle/Form/CategoryType.php
+++ src/AppBundle/Form/CategoryType.php
@@ -3,8 +3,6 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\Category;
-use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -19,21 +17,9 @@ class CategoryType extends AbstractType{
      */
 	public function buildform(FormBuilderInterface $builder, array $options)
 	{
-	    $categoryName = $options['categoryName'];
-
 		$builder
 		    ->add('name', TextType::class, [
 		        'label' => false,
-            ])
-            ->add('parent', EntityType::class, [
-                'class' => 'AppBundle\Entity\Category',
-                'query_builder' => function (EntityRepository $er) use ($categoryName) {
-                    return $er->createQueryBuilder('c')
-                        ->where('c.name != ?1')
-                        ->orderBy('c.name', 'ASC')
-                        ->setParameter(1, $categoryName);
-                },
-                'choice_label' => 'name',
             ])
             ->add('save', SubmitType::class, [
                 'attr'      => ['class' => 'button-link save'],
@@ -52,7 +38,6 @@ class CategoryType extends AbstractType{
     {
         $resolver->setDefaults([
             'data_class' => Category::class,
-            'categoryName' => null,
         ]);
     }
 
