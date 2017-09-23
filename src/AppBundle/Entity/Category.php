d09fcd7f0703f36a7a1eb8f2f632f3013bae5da3
Maria <maria.melnik.a@gmail.com>
2017-09-23 15:58:36 +0300

    New nested presentation of Category entity

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -2,17 +2,19 @@
 
 namespace AppBundle\Entity;
 
+use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\UniqueConstraint;
 
 /**
+ * @Gedmo\Tree(type="nested")
  * @ORM\Table(
  *     name="categories",
  *     uniqueConstraints={
  *      @UniqueConstraint(name="search_idx", columns={"name"})
  * })
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
  */
 class Category
 {
@@ -41,6 +43,43 @@ class Category
      * @OneToMany(targetEntity="AppBundle\Entity\Post", mappedBy="category")
      */
     private $posts;
+
+    /**
+     * @Gedmo\TreeLeft
+     * @ORM\Column(name="lft", type="integer")
+     */
+    private $lft;
+
+    /**
+     * @Gedmo\TreeLevel
+     * @ORM\Column(name="lvl", type="integer")
+     */
+    private $lvl;
+
+    /**
+     * @Gedmo\TreeRight
+     * @ORM\Column(name="rgt", type="integer")
+     */
+    private $rgt;
+
+    /**
+     * @Gedmo\TreeRoot
+     * @ORM\Column(name="root", type="integer", nullable=true)
+     */
+    private $root;
+
+    /**
+     * @Gedmo\TreeParent
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Category", mappedBy="parent")
+     * @ORM\OrderBy({"lft" = "ASC"})
+     */
+    private $children;
 
     /**
      * @param string $name
@@ -76,6 +115,26 @@ class Category
     public function getPosts(): array
     {
         return $this->posts->toArray();
+    }
+
+    /**
+     * @param Category|null $parent
+     *
+     * @return Category
+     */
+    public function setParent(Category $parent = null): ?Category
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParent()
+    {
+        return $this->parent;
     }
 
     /**
