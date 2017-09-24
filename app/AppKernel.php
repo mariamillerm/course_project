e17c8f691599cf7724e2fbd17c8a56d23928faa0
mimiminimalizm <maria.melnik.a@gmail.com>
2017-09-24 12:29:31 +0300

    Add elastic server

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Kitpages\SemaphoreBundle\KitpagesSemaphoreBundle(),
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
             new AppBundle\AppBundle(),
         ];
 
