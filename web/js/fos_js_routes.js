f6c980db0bf63c4595ae0892a7cfcb729cfbf362
mimiminimalizm <maria.melnik.a@gmail.com>
2017-09-25 05:58:01 +0300

    Add FOSJSRouting

---
+++ web/js/fos_js_routes.js
@@ -0,0 +1,1 @@
+fos.Router.setData({"base_url":"","routes":{"admin_post_edit":{"tokens":[["text","\/edit"],["variable","\/","\\d+","id"],["text","\/admin\/post"]],"defaults":[],"requirements":{"id":"\\d+"},"hosttokens":[],"methods":["DELETE","GET"],"schemes":[]}},"prefix":"","host":"localhost","scheme":"http"});
