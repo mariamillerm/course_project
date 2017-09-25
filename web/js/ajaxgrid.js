e530cc0c6190c25e8816c6d7db4006e49c0a2aad
mimiminimalizm <maria.melnik.a@gmail.com>
2017-09-25 06:03:50 +0300

    Create ajax-grid

--- web/js/ajaxgrid.js
+++ web/js/ajaxgrid.js
@@ -1,204 +1,186 @@
-!function ($) {
+$.fn.ajaxgrid = function(options) {
+    var root = this;
+    var table, tbody;
+    var headerRow;
+    var filters, pagination;
+    var request = {
+        rows: options.rowsPerPage,
+        page: 1
+    };
 
-    var Grid = function (element, options) {
-        this.$element = $(element)
-        this.options = $.extend({}, $.fn.grid.defaults, options)
-        this.ajaxUrl = this.options.ajaxUrl || this.ajaxUrl
-        this.limit = this.options.limit || this.limit
-        this.sortIndex = ''
-        this.sortOrder = 'ASC'
-        this.page = 1
-        this.totalRows = 0
-        this.totalPages = 0
-        this.listen()
-        this.ajax()
+    completeOptions(options);
+    createBaseElements();
+    sendInitRequest();
+
+    return root;
+
+    function completeOptions(options) {
+        if (!options.dataUrl) {
+            throw 'Wrong "dataUrl" exception';
+        }
+        options.sortableColumns = options.sortableColumns || [];
+        options.filterableColumns = options.filterableColumns || [];
+        options.rowsPerPage = options.rowsPerPage || 5;
     }
-
-    Grid.prototype = {
-
-        constructor: Grid
-
-       , ajax: function () {
-
-           var filters = this.$element.find('form').serializeArray(),
-               tbody = this.$element.find('table').find('tbody.row-result'),
-               emptyTbody = this.$element.find('table').find('tbody.row-empty'),
-               thisClass = this;
-
-           this.page = this.$element.find('#pagination #pagination-page').val();
-           this.limit = this.$element.find('#pagination #pagination-limit').val();
-
-           $.ajax({
-               url:this.ajaxUrl,
-               type: 'get',
-               data: {
-                   page: this.page,
-                   limit: this.limit,
-                   filters: filters,
-                   sort: this.sortIndex,
-                   sort_order: this.sortOrder,
-               }
-           })
-
-           dataType: 'JSON',
-            
-           thisClass.page = data.page,
-           thisClass.limit = data_page.limit,
-           thisClass.totalRows = data.row_count,
-           thisClass.totalPages = data.page_count,
-
-           thisClass.paginationProcess()
-
-           var html = ''
-
-           if (data.rows.length > 0) {
-               emptyTbody.hide()
-               $.each(data.rows, function (i, item) {
-                   html += '<tr>'
-                   $.each(item, function (i, value) {
-                       if (value == null) {
-                           value = ''
-                       }
-
-                       html += '<td>' + value + '</td>'
-                   })
-                   html += '</tr>'
-               })
-           } else {
-               emptyTbody.show()
-           }
-
-           tbody.html(html)
-       },
-
-        error:function (error) {
-            thisClass.gridUnlock()
-
-            emptyTbody.show()
-            tbody.html('')
-
-            alert('Error: ' + error.statusText)
-        }
+    function createBaseElements() {
+        filters = createElement('div', root).addClass('filters');
+        table = createElement('table', root);
+        headerRow = createElement('tr', table);
+        tbody = createElement('tbody', table);
+        pagination = createElement('div', root).addClass('pagination');
     }
-
-    return this
-}
-
-      listen:function All() {
-        this.$element.find('form').on('submit', $.proxy(this.submit, this))
-        this.$element.find('select').on('change', $.proxy(this.ajax, this))
-
-        this.$element.find('#refresh-button').on('click', $.proxy(this.ajax, this))
-        this.$element.find('#refresh-filters-button').on('click', $.proxy(this.refreshFilters, this))
-        this.$element.find('#row-filters-label th').on('click', $.proxy(this.processOrder, this))
-
-        this.$element.find('#pagination-back-button').on('click', $.proxy(this.paginationBack, this))
-        this.$element.find('#pagination-forward-button').on('click', $.proxy(this.paginationForward, this))
-
-    return this
-}
-
-        submit:function Submit() {
-            this.ajax()
-
-            return false
-        }
-
-        refreshFilters:function Refresh() {
-
-            $.each(this.$element.find('form'), function (i, form) {
-                form.reset()
-            })
-
-            $.each(this.$element.find('.date-input'), function (i, input) {
-                $(input).removeAttr('value')
-            })
-
-            this.ajax()
-
-            return this
-        }
-
-        gridLock:function Lock() {
-            this.$element.find('input, select, textarea, button').attr('disabled', true)
-            this.$element.css({opacity:0.5});
-
-            return this
-        }
-
-        gridUnlock:function Unlock() {
-            this.$element.find('input, select, textarea, button').attr('disabled', false)
-            this.$element.css({opacity:1});
-
-            return this
-        }
-
-        processOrder:function Order(event) {
-
-            var element = $(event.target)
-            sortIndex = element.data('index')
-
-            if (!sortIndex) {
-                return false
+    function sendInitRequest() {
+        $.getJSON(options.dataUrl, request, function(json) {
+            var columns = Object.getOwnPropertyNames(json.data[0]);
+            setHeader(headerRow, columns, json.columns, options.sortableColumns);
+            setData(tbody, json.data);
+            setPagination(pagination, Math.ceil(json.rows / options.rowsPerPage));
+            if (options.filterableColumns.length !== 0) {
+                setFilters(filters, options.filterableColumns, json.columns);
             }
+        });
+    }
 
-            if (this.sortIndex == sortIndex) {
-                if (this.sortOrder == 'DESC') {
-                    this.sortOrder = 'ASC'
-                } else {
-                    this.sortOrder = 'DESC'
+    function setHeader(headerRow, columns, translations, sortable) {
+        headerRow.empty();
+        setHeaderText(headerRow, columns, translations);
+        setHeaderSort(headerRow, sortable);
+    }
+    function setHeaderText(headerRow, columns, translations) {
+        for (var i = 0; i < columns.length; i++) {
+            if (columns[i] !== 'id') {
+                createElement('th', headerRow).text(translations[columns[i]]).attr('id', columns[i]);
+            }
+        }
+        createElement('th', headerRow).attr('id', '_edit');
+    }
+    function setHeaderSort(headerRow, sortable) {
+        sortable.forEach(function (column) {
+            headerRow.find('#' + column)
+                .addClass('sortable')
+                .click(function(){
+                    onSortClick(column);
+                });
+        });
+    }
+
+    function setData(tbody, data) {
+        tbody.empty();
+        for (var i = 0; i <data.length; i++) {
+            var row = createElement('tr', tbody);
+            setRow(row, data[i]);
+        }
+    }
+    function setRow(row, data) {
+        for (var dataCell in data) {
+            if (data.hasOwnProperty(dataCell) && dataCell !== 'id') {
+                createElement('td', row).text(data[dataCell]);
+            }
+        }
+        var editTd = createElement('td', row);
+        var editDiv = createElement('div', editTd).addClass('content-justify');
+        createElement('i', editDiv).addClass('glyphicon glyphicon-pencil glow edit').click(function () {
+            var win = window.open(Routing.generate(options.edit, {id: data.id}), '_blank');
+            win.focus();
+        });
+        createElement('i', editDiv).addClass('glyphicon glyphicon-remove glow remove').click(function () {
+            $.ajax(
+                Routing.generate(options.edit, {id: data.id}),
+                {
+                    type: 'DELETE',
+                    success: function () {
+                        $.getJSON(options.dataUrl, request, function (json) {
+                            setData(tbody, json.data);
+                            setPagination(pagination,
+                                Math.ceil(json.rows / options.rowsPerPage));
+                        });
+                    }
                 }
-            } else {
-                this.sortOrder = 'ASC'
-                this.sortIndex = sortIndex
+            );
+        });
+    }
+
+    function setPagination(pagination, pages) {
+        pagination.empty();
+        if (pages > 1) {
+            var page = request.page ? request.page : 0;
+            for (var i = 0; i < pages; i++) {
+                var button = createElement('button', pagination)
+                    .text(i + 1)
+                    .click(onPageClick.bind(i + 1));
+                if (i + 1 === +page) {
+                    button.addClass('selected');
+                }
             }
-
-            this.processOrderIcon(element)
-
-            this.ajax()
-
-            return this
         }
+    }
+    function setFilters(filters, filterable, translations) {
+        filters.empty();
+        var list = createElement('select', filters);
+        createElement('input', filters).attr('type', 'text');
+        createElement('button', filters).text(options.filter).click(onFilterClick);
+        filterable.forEach(function (column) {
+            createElement('option', list).text(translations[column]).attr('value', column);
+        });
 
-           paginationProcess: function Pagination() {
+    }
 
-               this.$element.find('#pagination #pagination-back-button').attr('disabled', false)
-               this.$element.find('#pagination #pagination-forward-button').attr('disabled', false)
+    function createElement(element, root) {
+        return $('<' + element + '>').appendTo(root);
+    }
 
-               if (this.page <= 1) {
-                   this.$element.find('#pagination #pagination-back-button').attr('disabled', true)
-               }
-
-               if (this.page >= this.totalPages) {
-                   this.$element.find('#pagination #pagination-forward-button').attr('disabled', true)
-               }
-
-               this.$element.find('#pagination #pagination-page').val(this.page)
-               this.$element.find('#pagination #pagination-total-pages').html(this.totalPages)
-               this.$element.find('#pagination #pagination-total').html(this.totalRows)
-               this.$element.find('#pagination #pagination-limit').val(this.limit)
-
-               this.$element.find('#pagination input').attr('disabled', false)
-
-               return this
-           }
-
-           paginationBack: function PaginationA() {
-               this.page--
-               this.$element.find('#pagination #pagination-page').val(this.page)
-
-               this.ajax()
-
-               return this
-           }
-
-           paginationForward: function PaginationB() {
-               this.page++
-               this.$element.find('#pagination #pagination-page').val(this.page)
-
-               this.ajax()
-
-               return this
-           }
-
-       
+    function onFilterClick() {
+        var pattern = $('.filters input').val();
+        if (pattern !== '') {
+            request.pattern = pattern;
+            request.filterbyfield = $('.filters select').val();
+            request.page = 1;
+        } else {
+            delete request.pattern;
+            delete request.filterbyfield;
+        }
+        $.getJSON(options.dataUrl, request, function (json) {
+            setData(tbody, json.data);
+            setPagination(pagination,
+                Math.ceil(json.rows / options.rowsPerPage));
+        });
+    }
+    function onPageClick() {
+        if (request.page !== this) {
+            request.page = this;
+            $.getJSON(options.dataUrl, request, function (json) {
+                setData(tbody, json.data);
+                setPagination(pagination,
+                    Math.ceil(json.rows / options.rowsPerPage));
+            });
+        }
+    }
+    function onSortClick(column) {
+        if (column === request.sortbyfield) {
+            if (request.order === 'asc') {
+                root.find('#' + column)
+                    .addClass('desc')
+                    .removeClass('asc');
+                request.order = 'desc';
+            } else {
+                root.find('#' + column)
+                    .removeClass('desc');
+                delete request.order;
+                delete request.sortbyfield;
+            }
+        } else {
+            root.find('#' + column)
+                .addClass('asc');
+            root.find('#' + request.sortbyfield)
+                .removeClass('asc')
+                .removeClass('desc');
+            request.sortbyfield = column;
+            request.order = 'asc';
+        }
+        $.getJSON(options.dataUrl, request, function (json) {
+            setData(tbody, json.data);
+            setPagination(pagination,
+                Math.ceil(json.rows / options.rowsPerPage));
+        });
+    }
+};
